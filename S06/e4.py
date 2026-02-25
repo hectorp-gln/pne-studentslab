5e3f74b4a062300822a09205ca34a05673b72c54
hectorp-gln <hectorpostigogalan@gmail.com>
2026-02-25 17:45:22 +0100

    Session 6 exercises

---
+++ S06/e4.py
@@ -0,0 +1,38 @@
+import termcolor
+
+class Seq:
+    def __init__(self, strbases):
+        self.bases = strbases
+        print("New sequence created!")
+
+    def __str__(self):
+        return self.bases
+
+    def len(self):
+        return len(self.bases)
+
+def print_seqs(sequen_list):
+    for seq in sequen_list:
+        print("Sequence " + str(sequen_list.index(seq)) + ": (Length: " + str(seq.len()) + ") " + str(seq))
+
+def generate_seq(pattern,n):
+    current_seq = ""
+    list_seq = []
+    i = 1
+    while i <= n:
+        current_seq += pattern
+        list_seq.append(Seq(current_seq))
+        i += 1
+    return list_seq
+
+seq_list1 = generate_seq("A",3)
+seq_list2 = generate_seq("AC",5)
+
+termcolor.cprint("List 1:","blue")
+for item in seq_list1:
+    termcolor.cprint(item,"blue")
+
+print()
+termcolor.cprint("List 2:","green")
+for item in seq_list2:
+    termcolor.cprint(item,"green")
