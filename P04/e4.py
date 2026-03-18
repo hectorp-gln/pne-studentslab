83abf7c47561f745c61f213d406c4a1d86fdd06a
hectorpg <hectorpostigogalan@gmail.com>
2026-03-18 18:06:16 +0100

    .

---
+++ P04/e4.py
@@ -0,0 +1,54 @@
+import socket
+import termcolor
+from pathlib import Path
+
+IP = "127.0.0.1"
+PORT = 8080
+
+
+def process_client(s):
+    req_raw = s.recv(2000)
+    req = req_raw.decode()
+
+    print("Message FROM CLIENT: ")
+
+    lines = req.split('\n')
+
+    req_line = lines[0]
+
+    print("Request line: ", end="")
+    termcolor.cprint(req_line, "green")
+
+    # -- Generate the response message
+    interest = req_line.split(" ")[1]
+    if interest == "/info/A" or interest == "/info/C" or interest == "/info/G" or interest == "/info/T":
+        body = Path("html" + interest + ".html").read_text()
+    else:
+        body = ""
+    status_line = "HTTP/1.1 200 OK\n"
+
+    header = "Content-Type: text/html\n"
+
+    header += f"Content-Length: {len(body)}\n"
+
+    response_msg = status_line + header + "\n" + body
+    cs.send(response_msg.encode())
+
+
+# -------------- MAIN PROGRAM
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+ls.bind((IP, PORT))
+ls.listen()
+
+while True:
+    print("Waiting for clients....")
+    try:
+        (cs, client_ip_port) = ls.accept()
+    except KeyboardInterrupt:
+        print("Server stopped!")
+        ls.close()
+        exit()
+    else:
+        process_client(cs)
+        cs.close()
